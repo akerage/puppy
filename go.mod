edb64775577734eec7aa07b4452f7c1da4a9c90e
Albert <albert@mayonnaisejar.com>
2023-09-24 18:47:35 -0500

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/akerage/puppy
 
 go 1.21.1
+
+require github.com/akerage/dog v0.0.0-20230924234121-f8a0093e90fc // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/akerage/dog"
+
 func Bark() string {
-    return "Woof"
+	return "Woof"
 }
 
 func Barks() string {
-    return "WoofWoofWoof"
+	return "WoofWoofWoof"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
