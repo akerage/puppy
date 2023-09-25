6310f37734c94f9bd2d0f4f22a45422e37d68e6e
Albert <albert@mayonnaisejar.com>
2023-09-24 19:21:19 -0500

    fourth

--- puppy.go
+++ puppy.go
@@ -17,3 +17,11 @@ func BigBark() string {
 func BigBarks() string {
 	return dog.WhenGrownUp(Barks())
 }
+
+func GetVersion1() string {
+    return "I'm from v0.0.1"
+}
+
+func GetVersion2() string {
+    return "I'm from v0.0.2"
+}
